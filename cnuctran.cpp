5930e4c3c6b743834e6893956f939501c8ddad1f
M. R. Omar <33319386+rabieomar92@users.noreply.github.com>
2022-01-06 23:26:45 +0800

    Add files via upload

---
+++ cnuctran.cpp
@@ -0,0 +1,16 @@
+﻿// cnuctran.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <simulation.h>
+#include <iostream>
+
+using namespace cnuctran;
+using namespace std;
+
+int main(int nargs, char** argv)
+{
+    nargs == 2? 
+        simulation::from_input(argv[1]) :
+        simulation::from_input(".\\input.xml");
+    return 0;
+
+}
