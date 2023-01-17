b4dc3e402e8fb8f551a492c71048b12966329c76
M. R. Omar <33319386+rabieomar92@users.noreply.github.com>
2023-01-17 09:24:18 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <simulation.h>
+#include <iostream>
+
+
+using namespace cnuctran;
+
+int main(int nargs, char** argv)
+{
+
+    nargs == 2? 
+        simulation::from_input(argv[1]) :
+        simulation::from_input(".\\input.xml");
+
+    return 0;
+
+}
+
