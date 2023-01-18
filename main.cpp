3abfabaeeb58f8ce070375d28afcad12a7b401f7
M. R. Omar <33319386+rabieomar92@users.noreply.github.com>
2023-01-18 11:08:28 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,16 @@
+/*
+      This file is part of the CNUCTRAN source code.
+      
+      @author   M. R. Omar (rabieomar@usm.my)
+      @license  MIT
+      @link     https://github.com/rabieomar92/cnuctran
+      
+      Copyright (c) 2023, Universiti Sains Malaysia.
+      
+      This is the MAIN program body. 
+      
+ */
+
 #include <simulation.h>
 #include <iostream>
 
